d949bd2a71955aea727f8156ace34b182962623b
Cazka <can_oe@me.com>
2020-08-24 21:18:34 +0200

    update deny.js

--- dieptool/src/protocol/lib/data/clientbound/deny.js
+++ dieptool/src/protocol/lib/data/clientbound/deny.js
@@ -2,10 +2,13 @@ module.exports = {
     id: 0x04,
     type: 'deny',
     parse(parser) {
-        return {};
+        const reason = parser.string();
+        return { reason };
     },
     build(builder) {
+        const { reason } = builder._packet.content;
         builder.vu(this.id);
+        builder.string(reason);
         return builder.out();
     },
 };
