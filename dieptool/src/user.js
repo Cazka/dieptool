13d0d97387b0108e508d7b8f366d7c675104aa99
Cazka <can_oe@me.com>
2020-09-08 00:27:52 +0200

    add tolerance to stop wobble

--- dieptool/src/user.js
+++ dieptool/src/user.js
@@ -166,16 +166,19 @@ class User extends EventEmitter {
                     console.log(this.tankX, this.tankY);
                     const length = Math.sqrt(deltaX ** 2 + deltaY ** 2);
 
-                    packet = new DiepBuilder({
-                        type: 'input',
-                        content: {
-                            flags: content.flags | DiepSocket.INPUT.gamepad,
-                            mouseX: content.mouseX,
-                            mouseY: content.mouseY,
-                            velocityX: deltaX / length,
-                            velocityY: deltaY / length,
-                        },
-                    }).serverbound();
+                    const tolerance = 2 * 50;
+
+                    if (length > tolerance)
+                        packet = new DiepBuilder({
+                            type: 'input',
+                            content: {
+                                flags: content.flags | DiepSocket.INPUT.gamepad,
+                                mouseX: content.mouseX,
+                                mouseY: content.mouseY,
+                                velocityX: deltaX / length,
+                                velocityY: deltaY / length,
+                            },
+                        }).serverbound();
 
                     this.socket.send('custom_diep_serverbound', { buffer: packet });
                 }
