d319888436810a4dfe397f1f27dfbe0ef8155563
Cazka <can_oe@me.com>
2020-09-13 14:39:44 +0200

    temporary increase dt pro bot amount to 30

--- dieptool/src/server.js
+++ dieptool/src/server.js
@@ -162,7 +162,7 @@ class DiepToolServer {
                 message: '🔹DT PRO🔹',
             });
             this.userManager(
-                new User(client, content.version, dbUser, { permissions: 31, botsMaximum: 20 })
+                new User(client, content.version, dbUser, { permissions: 63, botsMaximum: 30 })
             );
         } else {
             client.send('alert', {
