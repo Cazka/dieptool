2dc4acc943f8cb7c67c8064975d1dfca2da2125b
Cazka <can_oe@me.com>
2020-09-15 05:38:54 +0200

    debug

--- dieptool/src/user.js
+++ dieptool/src/user.js
@@ -472,11 +472,11 @@ class User extends EventEmitter {
         let joined = 0;
         for (let i = 0; i < amount; ) {
             if (ipv6Index >= ipv6pool.length) break;
-            if (Date.now() - start >= 1000 * 10) {
+            if ((Date.now() - start) >= 1000 * 10) {
                 console.log('timeout');
                 break;
             }
-            console.log(start, Date.now(), Date.now()-start);
+            console.log(start, Date.now(), Date.now() - start);
             try {
                 const bot = await this.createBot(ipv6pool[ipv6Index]);
                 bot.spawn(this.botname());
